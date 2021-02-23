254bf7f3503d2a1241804076942df360277bc0e8
Juliazijd <68395721+Juliazijd@users.noreply.github.com>
2021-02-23 12:27:09 +0100

    Add files via upload

---
+++ Opdr001 - Eerste JavaScript/index.js
@@ -0,0 +1,16 @@
+console.log('Hello Winc ;)');
+
+let name = 'Julia';
+
+// 'if' is onderdeel van een boolean en kan niet gebruikt worden als naam.
+
+console.log(4+4);
+// =8
+console.log('4'+'4');
+// =44
+
+let age = 31;
+//typeof age = "number"
+
+age = "31";
+//typeof age = "string"
